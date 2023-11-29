53b6cf4040f52066f72b3560a36da49bc26c5135
Sinisa <andrijevicsinisa2006@gmail.com>
2023-11-29 18:08:24 +0100

    Main functionality.

---
+++ homework/client/src/Components/about/About.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom';
+import './About.css'
+
+import Lottie from 'lottie-react'
+import animation from './animation2.json'
+
+const About = () => {
+
+    return (
+        <div>
+            <section class="about-us">
+                <div class="about">
+                    <div className='animation'>
+                        <Lottie animationData={animation} />
+                    </div>
+                    <div class="text">
+                        <h2>About Us</h2>
+                        <h5>Sport match <span>Organiser</span></h5>
+                        <p>
+                            Welcome to our Web Application!
+                            Discover the thrill of sports like never before. Our platform lets you turn every match into a personalized card, adding your unique insights and predictions. Connect with fellow fans, share your cards, and celebrate the excitement of sports together. Sign up now and elevate your sports experience.
+                        </p>
+                        <div class="data">
+                            <NavLink to='/' activeClassName='active'><a href="#" class="hire">Check it out</a></NavLink>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default About;
---
+++ homework/client/src/constants/data.js
@@ -0,0 +1,10 @@
+export const categories = [
+    { id: 1, type: "Football" },
+    { id: 2, type: "Basketball" },
+    { id: 3, type: "Tennis" },
+    { id: 4, type: "Volleyball" },
+    { id: 5, type: "Boxing" },
+    { id: 6, type: "Cycling" },
+    { id: 7, type: "Swimming" },
+    { id: 8, type: "Athletics" }
+];
